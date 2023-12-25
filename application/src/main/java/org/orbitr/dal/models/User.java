f6b9be21fd7ff8ff0bcb7e39c89692acc9e0289c
SABoev20 <SABoev20@codingburgas.bg>
2023-12-25 05:29:31 +0200

    Implement crud operations for user model

--- application/src/main/java/org/orbitr/dal/models/User.java
+++ application/src/main/java/org/orbitr/dal/models/User.java
@@ -1,4 +1,99 @@
 package org.orbitr.dal.models;
 
 public class User {
+    int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profilePicture = "";
+    private int credits;
+
+    public User() {}
+
+    public User(String firstName, String lastName, String username, String password, String email, String profilePicture, int credits) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.credits = credits;
+    }
+
+    public User(int id, String firstName, String lastName, String username, String password, String email, String profilePicture, int credits) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.profilePicture = profilePicture;
+        this.credits = credits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
 }
--- application/src/main/java/org/orbitr/dal/repositories/UserRepository.java
+++ application/src/main/java/org/orbitr/dal/repositories/UserRepository.java
@@ -1,4 +1,121 @@
 package org.orbitr.dal.repositories;
 
+import org.orbitr.dal.config.DatabaseConnection;
+import org.orbitr.dal.models.User;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+    public User createUser(User user) {
+        String sql = "INSERT INTO Users (FirstName, LastName, Username, Password, Email, ProfilePicture, Credits) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getUsername());
+            statement.setString(4, user.getPassword());
+            statement.setString(5, user.getEmail());
+            statement.setString(6, user.getProfilePicture());
+            statement.setInt(7, user.getCredits());
+
+            statement.executeUpdate();
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    user.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Database access error occurred.", e);
+        }
+        return user;
+    }
+
+    public User getUser(int id) {
+        String sql = "SELECT * FROM Users WHERE ID = ?";
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return new User(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("FirstName"),
+                        resultSet.getString("LastName"),
+                        resultSet.getString("Username"),
+                        resultSet.getString("Password"),
+                        resultSet.getString("Email"),
+                        resultSet.getString("ProfilePicture"),
+                        resultSet.getInt("Credits")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error reading user from the database.", e);
+        }
+        return null; // or throw an exception
+    }
+
+    public void updateUser(User user) {
+        String sql = "UPDATE Users SET FirstName = ?, LastName = ?, Username = ?, Password = ?, Email = ?, ProfilePicture = ?, Credits = ? WHERE ID = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, user.getFirstName());
+            statement.setString(2, user.getLastName());
+            statement.setString(3, user.getUsername());
+            statement.setString(4, user.getPassword());
+            statement.setString(5, user.getEmail());
+            statement.setString(6, user.getProfilePicture());
+            statement.setInt(7, user.getCredits());
+            statement.setInt(8, user.getId());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating user in the database.", e);
+        }
+    }
+
+    public void deleteUser(int id) {
+        String sql = "DELETE FROM Users WHERE ID = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error deleting user from the database.", e);
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT * FROM Users";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                users.add(new User(
+                        resultSet.getInt("ID"),
+                        resultSet.getString("FirstName"),
+                        resultSet.getString("LastName"),
+                        resultSet.getString("Username"),
+                        resultSet.getString("Password"),
+                        resultSet.getString("Email"),
+                        resultSet.getString("ProfilePicture"),
+                        resultSet.getInt("Credits")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error retrieving all users from the database.", e);
+        }
+        return users;
+    }
 }
